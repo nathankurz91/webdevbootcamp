1c439599412841b5c3fc0c359db68db9d7ff627b
nathankurz <nathankurz91@gmail.com>
2020-03-26 15:12:00 -0400

    landing page

---
+++ introToJS/app.js
@@ -0,0 +1,7 @@
+let name = prompt('What is your First Name?');
+let lastName = prompt('What is your last name?');
+let age = prompt('What is your age?');
+
+console.log('Your full name is ' + name + ' ' + lastName);
+console.log('You are ' + age + ' years old.');
+
